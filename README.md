d1222b11c6a8f78b7df5e064a1c44dfb98c58e2b
Rui Vieira <ruidevieira@googlemail.com>
2020-11-05 00:09:55 +0000

    Add REST server

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# python-template
-Python project templace
+# pmml-zoo
+![logo](docs/logo.png)
+
+A REST server to quickly create test PMML models.
+
+## About
+
+`pmml-zoo` allows you to quickly generate simulated data and create **test** PMML models by sending a JSON payload to a REST server, getting back the trained model.
+
+*What it is not*
+
+`pmml-zoo` **doesn't** aim at creating production models, it is intended to create models for smoke, integration and unit tests.
+
+## Usage
+
+As an example, let's create a linear regression.
+We can send the following JSON payload to `$SERVER/model/linearregression`:
+
+```json
+{"data": {
+    "size": 1000,
+    "inputs": [
+        {"name": "feature-1",
+         "type": "continuous",
+         "points": [[10.0, 30.0], [20.0, 40.0], [50, 15.0], [100, 16.0]]
+        },
+        {"name": "feature-2",
+        "type": "discrete",
+        "points": [[0, 3], [2, 4], [8, 1], [9, 16]]
+        },
+        {"name": "feature-3",
+        "type": "categorical",
+        "points": [["low", 2.0], ["medium", 4.0], ["high", 1.0]]
+        }
+        ],
+        "outputs": [
+            {"name": "output-1",
+            "type": "categorical",
+            "points": [["low", 2.0], ["medium", 4.0], ["high", 1.0]]}
+            ]
+        }
+}
+```
+
+### What is happening?
+
+Data is simulated by first creating an empirical distribution by interpolating the provided `points`.
+This empirical distribution is then sampled `size` times and that will be the variable data.
+
+A complete explanation is provided in the documentation.
+
+- `size` is the size of the dataset.
+- `points` is a list of data points to use to construct the interpolation, in the format `(value, weight)`. For instance a list of `[(1.0, 2.0), (2.0, 4.0)]` means that value `2.0` will more frequent.
+- `name` is the feature name, which be used in the PMML model
+- `type` can be one of `continuous`, `discrete` or `categorical`
+- `inputs` and `outputs` have the same format, with the obvious difference implied in the name.
+
+After sending the above payload, a response consisting of the PMML's XML is returned.
+
+## Supported models
+
+For now, these are the supported models:
+
+- Linear regression
+- Random forest classification
---
+++ src/pmmlzoo.py
@@ -0,0 +1,264 @@
+"""Tool to create test PMML models"""
+# INFO: Tool to create test PMML models
+# pylint: disable=R0903,R0913,C0103
+from abc import ABC, abstractmethod
+import random
+from typing import Union, Any, List, Tuple
+import uuid
+
+import numpy as np  # type: ignore
+from scipy import interpolate  # type: ignore
+from flask import Flask, request, Response
+import pandas as pd  # type: ignore
+from sklearn import preprocessing  # type: ignore
+from sklearn.linear_model import LinearRegression  # type: ignore
+from sklearn.pipeline import Pipeline  # type: ignore
+from sklearn.ensemble import RandomForestClassifier  # type: ignore
+from nyoka import skl_to_pmml  # type: ignore
+
+
+def _choose_interp_method(n_points: int) -> str:
+    if n_points >= 4:
+        method = "cubic"
+    elif n_points == 3:
+        method = "quadratic"
+    elif n_points == 2:
+        method = "slinear"
+    elif n_points == 1:
+        method = "zero"
+    else:
+        raise RuntimeError("Must have at least one point")
+    return method
+
+
+def create_continuous_histogram(start: int, end: int, data, size: int):
+    """Create a continuous histogram"""
+    x = [p[0] for p in data]
+    y = [p[1] for p in data]
+
+    n_points = len(data)
+
+    f = interpolate.interp1d(
+        x, y, fill_value="extrapolate", kind=_choose_interp_method(n_points)
+    )
+
+    xnew = np.linspace(start, end, size)
+    ynew = f(xnew)
+    result = np.empty([size, 2])
+    result[:, 0] = xnew
+    result[:, 1] = ynew
+
+    return result
+
+
+def sample_from_histogram(histogram, size):
+    """Sample data points from the interpolated histogram"""
+    weights = histogram[:, 1].astype(float)
+    return random.choices(population=histogram[:, 0], weights=weights, k=size)
+
+
+def create_discrete_histogram(start: int, end: int, data, size: int):
+    """Create a discrete series"""
+    result = create_continuous_histogram(start=start, end=end, data=data, size=size)
+    result[:, 0] = result[:, 0].astype(int)
+
+    return result
+
+
+def create_categorical_histogram(data):
+    """Create a categorical series"""
+    return np.array(data)
+
+
+class Variable(ABC):
+    """Variable abstract class"""
+
+    def __init__(self, name, size, points):
+        self.name = name
+        self.points = points
+        self.size = size
+
+    @abstractmethod
+    def generate_data(self):
+        """Generate the data associated with this variable"""
+
+
+class ContinuousVariable(Variable):
+    """Continuous variable"""
+
+    def __init__(self, name, size, points, start, end):
+        super().__init__(name, size, points)
+        self.start = start
+        self.end = end
+
+    def generate_data(self):
+        histogram = create_continuous_histogram(
+            start=self.start,
+            end=self.end,
+            data=self.points,
+            size=self.size,
+        )
+        return sample_from_histogram(histogram, self.size)
+
+    def __repr__(self):
+        return f"""ContinuousVariable{{
+            name={self.name}, start={self.start}, end={self.end}, size={self.size}
+            }}"""
+
+
+class DiscreteVariable(Variable):
+    """Continuous variable"""
+
+    def __init__(self, name, size, points, start, end):
+        super().__init__(name, size, points)
+        self.start = start
+        self.end = end
+
+    def generate_data(self):
+        histogram = create_discrete_histogram(
+            start=self.start,
+            end=self.end,
+            data=self.points,
+            size=1000,
+        )
+        return sample_from_histogram(histogram, self.size)
+
+    def __repr__(self):
+        return f"""DiscreteVariable{{
+            name={self.name}, start={self.start}, end={self.end}, size={self.size}
+            }}"""
+
+
+class CategoricalVariable(Variable):
+    """Continuos variable"""
+
+    def generate_data(self):
+        histogram = create_categorical_histogram(data=self.points)
+        return sample_from_histogram(histogram, self.size)
+
+    def __repr__(self):
+        return f"CategoricalVariable{{name={self.name}, size={self.size}}}"
+
+
+# Model generation
+TVariable = Union[ContinuousVariable, DiscreteVariable, CategoricalVariable]
+Inputs = List[TVariable]
+Outputs = Inputs
+
+
+def __create_dataframe(variables: List[TVariable]) -> pd.DataFrame:
+    df = pd.DataFrame()
+    for variable in variables:
+        # drop indices
+        if isinstance(variable, CategoricalVariable):
+            le = preprocessing.LabelEncoder()
+            # encode categorical variables
+            df[variable.name] = le.fit_transform(variable.generate_data())
+        elif isinstance(variable, DiscreteVariable):
+            df[variable.name] = variable.generate_data()
+            df[variable.name] = df[variable.name].astype(int)
+        else:
+            df[variable.name] = variable.generate_data()
+    return df
+
+
+def generate_linear_regression(inputs: Inputs, outputs: Outputs):
+    """Generate a linear regression from the supplied variables"""
+    input_df = __create_dataframe(inputs)
+    output_df = __create_dataframe(outputs)
+    pipeline_obj = Pipeline([("model", LinearRegression())])
+    pipeline_obj.fit(input_df, output_df)
+    temporary_file = f"/tmp/{uuid.uuid4()}.pmml"
+    skl_to_pmml(pipeline_obj, input_df.columns, output_df.columns, temporary_file)
+    with open(temporary_file, "r") as file:
+        data = file.read()
+    return data
+
+
+def generate_random_forest(inputs: Inputs, outputs: Outputs):
+    """Generate a random forest from the supplied variables"""
+    input_df = __create_dataframe(inputs)
+    output_df = __create_dataframe(outputs)
+    pipeline_obj = Pipeline([("model", RandomForestClassifier())])
+    pipeline_obj.fit(input_df, output_df)
+    temporary_file = f"/tmp/{uuid.uuid4()}.pmml"
+    skl_to_pmml(pipeline_obj, input_df.columns, output_df.columns, temporary_file)
+    with open(temporary_file, "r") as file:
+        data = file.read()
+    return data
+
+
+def build_variable(json_str, data_size: int) -> TVariable:
+    """Parse variable information from JSON"""
+    name: str = json_str["name"]
+    points = json_str["points"]
+    variable_type: str = json_str["type"]
+    if variable_type != "categorical":
+        start = json_str.get("start", min([p[0] for p in points]))
+        end = json_str.get("end", max([p[0] for p in points]))
+    else:
+        start = 0.0
+        end = 0.0
+
+    if variable_type == "continuous":
+        variable_obj: TVariable = ContinuousVariable(
+            name=name,
+            start=start,
+            end=end,
+            size=data_size,
+            points=points,
+        )
+    elif variable_type == "discrete":
+        variable_obj = DiscreteVariable(
+            name=name,
+            start=start,
+            end=end,
+            size=data_size,
+            points=points,
+        )
+    else:
+        variable_obj = CategoricalVariable(
+            name=name,
+            size=data_size,
+            points=points,
+        )
+    return variable_obj
+
+
+def parse_payload(payload: Any) -> Tuple[Inputs, Outputs]:
+    """Parse JSON payload into inputs and outputs"""
+    data_size = int(payload["data"]["size"])
+    # get inputs
+    inputs = []
+    for _input in payload["data"]["inputs"]:
+        inputs.append(build_variable(_input, data_size))
+    outputs = []
+    for _output in payload["data"]["outputs"]:
+        outputs.append(build_variable(_output, data_size))
+    return (inputs, outputs)
+
+
+# REST server
+app = Flask(__name__)
+
+
+@app.route("/model/linearregression", methods=["POST"])
+def linear_regression():
+    """Linear regression model endpoint"""
+    inputs, outputs = parse_payload(request.json)
+    model = generate_linear_regression(inputs, outputs)
+
+    return Response(model, mimetype="text/xml")
+
+
+@app.route("/model/randomforest", methods=["POST"])
+def random_forest():
+    """Random forest model endpoint"""
+    inputs, outputs = parse_payload(request.json)
+    model = generate_random_forest(inputs, outputs)
+
+    return Response(model, mimetype="text/xml")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
